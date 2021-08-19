45fadcc9ada80ce4ca37d6d496b53d5296bf79c2
Ismael Boro <isborrr@gmail.com>
2021-08-19 13:04:53 -0400

    Updates

---
+++ precache-manifest.b4c1d5f295334320d8b0db88ad1f1d20.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e688fba57c6fd82e3060ee139affcc4",
+    "url": "/isborrr/porfolio.git/index.html"
+  },
+  {
+    "revision": "1c634a7b93018eea8b12",
+    "url": "/isborrr/porfolio.git/static/css/main.a1bf20c3.chunk.css"
+  },
+  {
+    "revision": "d5b2dee0796d4911c99f",
+    "url": "/isborrr/porfolio.git/static/js/2.e3b66748.chunk.js"
+  },
+  {
+    "revision": "d3aa841a02b937500c0c",
+    "url": "/isborrr/porfolio.git/static/js/3.f8d1debf.chunk.js"
+  },
+  {
+    "revision": "1c634a7b93018eea8b12",
+    "url": "/isborrr/porfolio.git/static/js/main.a8038440.chunk.js"
+  },
+  {
+    "revision": "6664bd9d5dfded87951d",
+    "url": "/isborrr/porfolio.git/static/js/runtime-main.4ff2395e.js"
+  },
+  {
+    "revision": "23479abf791df8fd808336732b07fe3b",
+    "url": "/isborrr/porfolio.git/static/media/Bootcamp.23479abf.jpeg"
+  },
+  {
+    "revision": "3f6e5bbbb4259dc6cfe567c0ad56527f",
+    "url": "/isborrr/porfolio.git/static/media/Certificate.3f6e5bbb.jpeg"
+  },
+  {
+    "revision": "8575c70e0ccdb8c83d23952ec4a5b0cd",
+    "url": "/isborrr/porfolio.git/static/media/Email.8575c70e.jpeg"
+  },
+  {
+    "revision": "bec6b968b7341018f1afdc8579c5fc6b",
+    "url": "/isborrr/porfolio.git/static/media/Job1icon.bec6b968.png"
+  },
+  {
+    "revision": "feabe97076e5008d1996deeb8aa3a33f",
+    "url": "/isborrr/porfolio.git/static/media/Job2icon.feabe970.png"
+  },
+  {
+    "revision": "7d5ad40fa2905a7f1989a6b75a4debad",
+    "url": "/isborrr/porfolio.git/static/media/Phone.7d5ad40f.jpeg"
+  },
+  {
+    "revision": "28fc2d75ced638dcf3892a37df0cf8a4",
+    "url": "/isborrr/porfolio.git/static/media/ProfileBgrd.28fc2d75.png"
+  },
+  {
+    "revision": "e7ecd839748d4ffd41f0221694cbf721",
+    "url": "/isborrr/porfolio.git/static/media/ProfileImg.e7ecd839.JPG"
+  },
+  {
+    "revision": "f72fb14b993a7f90ab80a29059d6fba6",
+    "url": "/isborrr/porfolio.git/static/media/background.f72fb14b.jpeg"
+  },
+  {
+    "revision": "5f3f234b068c607ad5326533bf961324",
+    "url": "/isborrr/porfolio.git/static/media/bgrd.5f3f234b.png"
+  },
+  {
+    "revision": "63db88c3709cd368f6fd3898f2b500c1",
+    "url": "/isborrr/porfolio.git/static/media/certificate-icon.63db88c3.png"
+  },
+  {
+    "revision": "e49ca2bdc3da1519c2b9ee79c527ecac",
+    "url": "/isborrr/porfolio.git/static/media/goldStarRounded.e49ca2bd.jpeg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/isborrr/porfolio.git/static/media/logo.06e73328.svg"
+  }
+]);
